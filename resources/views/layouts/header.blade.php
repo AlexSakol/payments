e3ddf443b4187e0745ac447f557697b119c69eba
Aleksey Sakalinski <aos-1995@yandex.com>
2024-01-07 22:13:57 +0300

    Enable login and logout

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,13 +4,34 @@
 
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                 <li><a href="/" class="nav-link px-2 text-secondary">Главная</a></li>
+                @auth()
                 <li><a href="#" class="nav-link px-2 text-white">Мои платежи</a></li>
+                @endauth
             </ul>
-
+            @guest()
             <div class="text-end">
-                <button type="button" class="btn btn-outline-light me-2">Войти</button>
-                <button type="button" class="btn btn-warning">Зарегистрироваться</button>
+                <a href="{{route('login')}}" class="btn btn-outline-light me-2">Войти</a>
+                <a href="{{route('register')}}" class="btn btn-warning">Зарегистрироваться</a>
             </div>
+            @else
+                <div class="text-end">
+                    <div class="dropdown">
+                        <a class="btn btn-secondary dropdown-toggle" href="{{route('dashboard')}}" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Кабинет
+                        </a>
+
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="{{route('dashboard')}}">Кабинет</a></li>
+                            <li>
+                                <form method="post" action="{{route('logout')}}">
+                                    @csrf
+                                    <input type="submit" class="dropdown-item" value="Выйти">
+                                </form>
+                            </li>
+                        </ul>
+                </div>
+                </div>
+            @endguest
         </div>
     </div>
 </header>
