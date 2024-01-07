129ad5fe1e75f534c392f4ff9fb80ec9595a8df4
Aleksey Sakalinski <aos-1995@yandex.com>
2024-01-07 21:44:38 +0300

    Add header

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,16 @@
+<header class="p-3 text-bg-dark">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="/" class="nav-link px-2 text-secondary">Главная</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Мои платежи</a></li>
+            </ul>
+
+            <div class="text-end">
+                <button type="button" class="btn btn-outline-light me-2">Войти</button>
+                <button type="button" class="btn btn-warning">Зарегистрироваться</button>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.index')
+@section('title')Главная@endsection
+@section('content')
+    <p>
+        Система контроля платежей
+    </p>
+@endsection
